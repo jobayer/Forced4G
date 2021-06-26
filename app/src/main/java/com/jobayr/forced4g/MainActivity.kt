37e4bd82927ab03c34aaca97be20d34567712003
Md Jobayer <jobayr45@gmail.com>
2021-06-26 18:00:53 +0600

    Initial Commit

---
+++ app/src/main/java/com/jobayr/forced4g/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.jobayr.forced4g
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        try {
+            val intent = Intent("android.intent.action.MAIN")
+            intent.setClassName("com.android.phone", "com.android.phone.settings.RadioInfo")
+            startActivity(intent)
+        } catch (e: Exception) {
+            Toast.makeText(this, "Device Not Supported", Toast.LENGTH_SHORT).show()
+        }
+        finishAffinity()
+    }
+}
